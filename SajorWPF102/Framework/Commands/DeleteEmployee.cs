604c3a6823dac6a8f09bd81654cb52113ee1e752
sajor24 <143572576+sajor24@users.noreply.github.com>
2026-02-26 20:45:24 +0800

    Add files via upload

---
+++ SajorWPF102/Framework/Commands/DeleteEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Commands;
+using Domain.Models;
+using Framework.Extensions;
+
+namespace Framework.Commands
+{
+
+    public class DeleteEmployee : IDeleteEmployee
+    {
+        private readonly Repository _repository;
+
+        public DeleteEmployee(Repository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ExecuteAsync(EmployeeModel model)
+        {
+            var parameters = model.ToDeleteEmployeeDynamicParameters();
+            await _repository.SaveDataAsync("DefaultConnection", "[dbo].[DeleteEmployee]", parameters);
+        }
+    }
+}
