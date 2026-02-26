66f419cea0f1a6c8efbbf77ddfb498a84cd1cce5
sajor24 <143572576+sajor24@users.noreply.github.com>
2026-02-26 20:46:52 +0800

    Add files via upload

---
+++ SajorWPF102/Domain/Commands/IUpdateEmployee.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Commands
+{
+    public interface IUpdateEmployee
+    {
+        Task ExecuteAsync(EmployeeModel model);
+
+    }
+}
