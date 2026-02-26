6b18c09d8de85f377fd5cb3a37e56e47503e02c5
sajor24 <143572576+sajor24@users.noreply.github.com>
2026-02-26 21:48:33 +0800

    Add files via upload

---
+++ SajorWPF102/Domain/Commands/IDeleteEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Commands;
+using Domain.Models;
+
+namespace Domain.Commands
+{
+    public interface IDeleteEmployee
+    {
+        Task ExecuteAsync(EmployeeModel model);
+
+    }
+}
